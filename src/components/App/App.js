1b29cc47f4ff3ea23d021d617dabd9ed10b6d125
mifodiy911 <mifodiy911@gmail.com>
2024-03-05 18:03:26 +0200

    add ProductList and ShopList to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
 import Header from '../Header/Header';
+import ShopList from '../ShopList/ShopList';
+import ProductList from '../ProductList/ProductList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header/>
+      <div className='app__wrapper'>
+        <ShopList />
+        <ProductList />
+      </div>
+
     </div>
   );
 }
